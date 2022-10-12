8ce6ba70d4b6e2b2a4c916a2e1ea225d5ff84c92
邓杨江浩 <dengyangjianghao@nutri-packs.com>
2022-10-12 16:26:16 +0800

    1

--- main.go
+++ main.go
@@ -12,20 +12,20 @@ import (
 // Golang RPC 的实现需要 5 个步骤
 // 1. 定义一个服务结构
 // 2. 为这个服务结构定义几个服务方法，每个方法接受两个参数和返回 error 类型
-// 3. 使用 rpc.Register() 方法注册 「服务结构」 的实例
+// 3. 使用 rpc.RegisterName() 方法注册 「服务结构」 的实例
 // 4. 监听套接字
 // 5. 为每一个套接字调用 jsonrpc.ServerConn(conn) 方法
 
 type RocketService struct {
 }
 
-// 定义 MathService 所需要的参数，一般是两个，int 类型
+// 定义 RocketService 所需要的参数，一般是两个，string 类型
 type Args struct {
 	Topic, Message string
 }
 
 // 2.
-// 实现加法服务，加法需要两个参数
+// 实现延时消息，需要两个参数
 // 所有的 jsonrpc 方法只有两个参数，第一个参数用于接收所有参数，
 // 第二个参数用于处理返回结果，是一个指针
 // 所有的 jsonrpc 都只有一个返回值，error,用于指示是否发生错误
@@ -34,11 +34,13 @@ func (that *RocketService) Delay(args Args, reply *bool) error {
 	return nil
 }
 
+// 实现即时推送服务
 func (that *RocketService) Simple(args Args, reply *bool) error {
 	*reply = simple.PushSimple(args.Topic, args.Message)
 	return nil
 }
 
+// Hello
 func (that *RocketService) Hello(request string, reply *string) error {
 	*reply = "hello " + request
 	return nil
